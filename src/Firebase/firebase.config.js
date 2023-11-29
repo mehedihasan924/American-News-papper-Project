983eae4c3b90eb3f8418c5fff703c24c5334d9c9
Mehedi-Hasan <mdhasanmehedi924@gmail.com>
2023-11-28 22:21:34 -0800

    Protect ENV File

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log("environment variable" , import.meta.env.VITE_APIKEY);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCvfU38JbSspjxh70Bvr2QU8EtNJspaaPc",
-  authDomain: "american-news-papper-project.firebaseapp.com",
-  projectId: "american-news-papper-project",
-  storageBucket: "american-news-papper-project.appspot.com",
-  messagingSenderId: "469126802422",
-  appId: "1:469126802422:web:a71136270efe8869a7b5d7"
+  apiKey:import.meta.env.VITE_APIKEY ,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN ,
+  projectId:import.meta.env.VITE_PROJECTID ,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId:import.meta.env.VITE_APPID 
 };
 
 // Initialize Firebase
